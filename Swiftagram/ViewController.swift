6250a882a6e4f31df926debf5eefc5b0b644d29d
Mark Barrasso <mark.barrasso@live.com>
2015-01-04 12:00:09 -0500

    set up Parse implementation. created, query, updated and save sample parse data

--- Swiftagram/ViewController.swift
+++ Swiftagram/ViewController.swift
@@ -10,16 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set up Parse application
+        Parse.setApplicationId("wabvjYL0hvJHpUDFGNceYAM6f7zrlS9s3qRBqn9O",
+              clientKey: "JHBuhVrPu6neUZwwydgKQMqDOSIlq8eCdtzRKDtA")
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
+
+
+
+
+/********
+
+//////////////////////////////////////////////////
+// CREATING AND SAVING NEW PARSE CLASS + OBJECT //
+//////////////////////////////////////////////////
+
+var score = PFObject(className: "score")
+score.setObject("Mark", forKey: "name")
+score.setObject(95, forKey: "number")
+score.saveInBackgroundWithBlock {
+(success: Bool!, error: NSError!) -> Void in
+
+if success == true
+{
+NSLog("Score created with ID: %@", score.objectId)
+}
+else
+{
+NSLog("Error: %@", error);
+}
+
+}
+********/
+
+
+
+/********
+
+/////////////////////////////////////////////////////////
+// QUERY, UPDATING AND SAVING NEW PARSE CLASS + OBJECT //
+/////////////////////////////////////////////////////////
+
+var query = PFQuery(className: "score")
+query.getObjectInBackgroundWithId("AtKCkKXwEb") {
+(score: PFObject!, error: NSError!) -> Void in
+
+if error == nil
+{
+NSLog("Got Object with ID: %@", score)
+score["name"] = "Markos"
+score["number"] = 1236
+score.saveInBackgroundWithBlock {
+(success: Bool!, error: NSError!) -> Void in
+
+if success == true
+{
+NSLog("Score updated with ID: %@", score.objectId)
+}
+else
+{
+NSLog("Error: %@", error);
+}
+}
+}
+else
+{
+NSLog("Error: %@", error);
+}
+}
+
+********/
 

3ec82891b4846cfc7dacc950e8553d00313d9df1
Mark Barrasso <mark.barrasso@live.com>
2015-01-07 19:53:52 -0800

    retrieves all users and displays on user table view

--- Swiftagram/UserTableViewController.swift
+++ Swiftagram/UserTableViewController.swift
@@ -10,17 +10,58 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
     
+    // cell array
+    var users = [""]
+    
     // MARK: View Initialization
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        var userQuery = PFUser.query()
+        userQuery.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
+            
+            
+            self.users.removeAll(keepCapacity: true)
+            
+            for object in objects {
+                
+                var user:PFUser = object as PFUser
+                
+                self.users.append(user.username)
+            }
+            
+            self.tableView.reloadData()
+        })
+        
+        
     }
     
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = users[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
     }
 
 }

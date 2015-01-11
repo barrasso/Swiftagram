483ddb4beffbe927e36644eec23c0cc9e0db0be4
Mark Barrasso <mark.barrasso@live.com>
2015-01-11 00:57:36 -0500

    implemented saving of images to parse with error checking and decent ux

--- Swiftagram/PostViewController.swift
+++ Swiftagram/PostViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
     // MARK: Variables
     
+    // photo selected flag
     var photoSelected:Bool = false
+    
+    // Activity spinner indicator
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
     // MARK: Outlets
     
@@ -24,6 +28,14 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        // set text field delegate
+        shareTextField.delegate = self
+        
+        // reset flag, caption and image
+        photoSelected = false
+        imageToPost.image = UIImage(named: "blank-woman-placeholder.png")
+        shareTextField.text = ""
     }
     
     override func didReceiveMemoryWarning()
@@ -58,17 +70,78 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
         // check if user has chosen an image
         if (photoSelected == false) {
-            error = "Please select an image to post"
+            error = "Please choose an image."
         }
         
-        // check for error string
+        // check for error
         if (error != "") {
-            displayAlert("Error posting image", error: error)
+            displayAlert("Error Posting Image", error: error)
+        } else {
+            
+            // inital activity indicator setup
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0.0, 0.0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true;
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            
+            // add to view and start animation
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            
+            // begin ignoring user interaction
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            // define new parse object
+            var post = PFObject(className: "Post")
+            post["title"] = shareTextField.text
+            post["username"] = PFUser.currentUser().username
+            
+            // save post with image if success
+            post.saveInBackgroundWithBlock{(success: Bool!, error: NSError!) -> Void in
+                
+                if success == false {
+                    // stop animation and end ignoring events
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    self.displayAlert("Could Not Post Image", error: "Please try again later.")
+                } else {
+                    
+                    // set image data
+                    let imageData = UIImagePNGRepresentation(self.imageToPost.image)
+                    
+                    // save the image to PFFile
+                    let imageFile = PFFile(name: "image.png", data: imageData)
+                    
+                    // add to post
+                    post["imageFile"] = imageFile
+                    
+                    // save post with image if success
+                    post.saveInBackgroundWithBlock{(success: Bool!, error: NSError!) -> Void in
+                        
+                        // stop animation and end ignoring events
+                        self.activityIndicator.stopAnimating()
+                        UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                        
+                        if success == false {
+                            self.displayAlert("Could Not Post Image", error: "Please try again later.")
+                        } else {
+                            self.displayAlert("Image Posted", error: "Your image has been posted successfully!")
+                            
+                            // reset flag, caption and image
+                            self.photoSelected = false
+                            self.imageToPost.image = UIImage(named: "blank-woman-placeholder.png")
+                            self.shareTextField.text = ""
+                        }
+                    }
+                }
+            }
+            
         }
     }
     
     
-    // MARK: Image Picker Delegate Methods
+    // MARK: Image Picker Delegate Functions
     
     // user did finishing picking an image
     func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!)
@@ -83,6 +156,13 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
         // set photo selected flag to true
         photoSelected = true
+    }
+    
+    // MARK: Text Field Delegate Functions
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        shareTextField.resignFirstResponder()
+        return true
     }
     
     // MARK: Alert Functions

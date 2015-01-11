a70d8ffc2d8410126eeeba9e1f1ffc3d0c4d0a9a
Mark Barrasso <mark.barrasso@live.com>
2015-01-11 00:03:18 -0500

    setup post image view controller

---
+++ Swiftagram/PostViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PostViewController.swift
+//  Swiftagram
+//
+//  Created by Mark on 1/10/15.
+//  Copyright (c) 2015 MEB. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var imageToPost: UIImageView!
+    @IBOutlet weak var shareTextField: UITextField!
+    
+    // MARK: Actions
+    @IBAction func chooseImage(sender: AnyObject)
+    {
+        
+    }
+    
+    @IBAction func postImage(sender: AnyObject)
+    {
+        
+    }
+    
+    // MARK: View Initialization
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+
+}

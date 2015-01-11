5b9dc02685aada882fa9977821bdccad9caf8d0e
Mark Barrasso <mark.barrasso@live.com>
2015-01-11 00:05:51 -0500

    implemented image picker controller methods

--- Swiftagram/PostViewController.swift
+++ Swiftagram/PostViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Outlets
     @IBOutlet weak var imageToPost: UIImageView!
@@ -17,12 +17,42 @@ class PostViewController: UIViewController {
     // MARK: Actions
     @IBAction func chooseImage(sender: AnyObject)
     {
+        // init picker controller
+        var image = UIImagePickerController()
         
+        // set delegate
+        image.delegate = self
+        
+        // access photo library
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        // access camera
+        //image.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        // allows user to edit picture
+        image.allowsEditing = false
+        
+        // present the picker controller
+        self.presentViewController(image, animated: true, completion: nil)
     }
     
     @IBAction func postImage(sender: AnyObject)
     {
         
+    }
+    
+    
+    // MARK: Image Picker Delegate Methods
+    
+    // user did finishing picking an image
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!)
+    {
+        NSLog("Picked Image")
+        
+        // close image picker
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        // set chosen image to UIImage
+        imageToPost.image = image
     }
     
     // MARK: View Initialization
